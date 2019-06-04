02fd3f344291dcced2840649f37d2e61429aa3fc
Matheus Fróes <matheusfroesmarques@gmail.com>
2019-06-04 06:37:52 -0300

    Initial Commit

---
+++ SwiftUISample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUISample
+//
+//  Created by Matheus Fróes on 04/06/19.
+//  Copyright © 2019 Matheus Fróes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
